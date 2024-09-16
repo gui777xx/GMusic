5a242a5ea545663e0909a3ea2f0b505d79df68d1
gui777x <guizin.clementino@gmail.com>
2024-09-16 11:38:40 -0300

    feito

--- GMusic/screens/MusicPlayer.js
+++ GMusic/screens/MusicPlayer.js
@@ -1,46 +1,105 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import {
-    Dimensions,
-    Image,
-    SafeAreaView, 
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View } from 'react-native'
+  Dimensions,
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native'
 import Slider from '@react-native-community/slider'
 import { Ionicons } from '@expo/vector-icons'
+import songs from '../model/data'
 
 const { width, height } = Dimensions.get('window')
 
 const MusicPlayer = () => {
+
+  const renderSongs = ({ item, index}) => {
+    return (
+      <View style={styles.mainImageWrapper}>
+      <View style={[styles.imageWrapper, styles.elevation]} >
+      <Image
+        source={ item.artwork }
+        style={styles.musicImage}
+      />
+      </View>
+    </View>
+    )
+  }
+
   return (
-  <SafeAreaView style={styles.container}>
-        <View style={styles.main}>
-          <View style={[styles.imageWrapper, styles.elevation]}/>
-            <Image
-            source={require('../assets/img/gallo1.png')}
-            style={styles.musicImage}
-            />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.main}>
+      
+      <FlatList
+        renderItem={renderSongs}
+        data={songs}
+        keyExtractor={ item => item.id }
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        onscroll={() => {}}
+      />
+        <View>
+          <Text style={[styles.songContent, styles.songTitle]}>
+            Titulo da Musica
+          </Text>
+          <Text style={[styles.songContent, styles.songArtist]}>
+            Autor da Musica
+          </Text>
+        </View>
+        <View>
+          <Slider
+            style={styles.progressBar}
+            value={10}
+            minimumValue={0}
+            maximumValue={100}
+            thumbTintColor='#FFD369'
+            minimumTrackTintColor='#FFD369'
+            maximumTrackTintColor='#FFF'
+            onSlidingComplete={() => { }}
+
+          />
+          <View style={styles.progressLevelDuration}>
+            <Text style={styles.progressLabeltext}>00.00</Text>
+            <Text style={styles.progressLabeltext}>00.00</Text>
+          </View>
+        </View>
+        <View style={styles.musicControlsContainer}>
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name='play-skip-back-outline' size={35} color="#FFD369" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name='pause-circle' size={75} color="#FFD369" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name='play-skip-forward-outline' size={45} color="#FFD369" />
+          </TouchableOpacity>
+        </View>
+
         </View>
         <View style={styles.footer}>
           <View style={styles.iconWrapper}>
             <TouchableOpacity>
-              <Ionicons name='heart-outline' size={30} color="#888888"/>
+              <Ionicons name='heart-outline' size={30} color="#888888" />
             </TouchableOpacity>
             <TouchableOpacity>
-              <Ionicons name='repeat' size={30} color="#888888"/>
+              <Ionicons name='repeat' size={30} color="#888888" />
             </TouchableOpacity>
             <TouchableOpacity>
-              <Ionicons name='share-outline' size={30} color="#888888"/>
+              <Ionicons name='share-outline' size={30} color="#888888" />
             </TouchableOpacity>
             <TouchableOpacity>
-              <Ionicons name='ellipsis-horizontal' size={30} color="#888888"/>
+              <Ionicons name='ellipsis-horizontal' size={30} color="#888888" />
             </TouchableOpacity>
           </View>
-
         </View>
-      <StatusBar style='light'/>
+        <StatusBar style='light' />
     </SafeAreaView>
   )
 }
@@ -48,26 +107,93 @@ const MusicPlayer = () => {
 export default MusicPlayer
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#222831',
+  container: {
+    flex: 1,
+    backgroundColor: '#222831',
+  },
+
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  mainImageWrapper: {
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageWrapper: {
+    width: 340,
+    height: 360,
+    marginVertical: 30,
+  },
+
+  musicImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 15,
+  },
+
+  songContent: {
+    textAlign: 'center',
+    color: '#EEEEEE'
+  },
+
+  songTitle: {
+    fontSize: 18,
+    fontWeight: 600,
+  },
+
+  songArtist: {
+    fontSize: 16,
+    fontWeight: 300,
+  },
+
+  progressBar: {
+    width: 320,
+    height: 40,
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+
+  progressLevelDuration: {
+    width: 340,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  progressLabeltext: {
+    color: '#fff',
+    fontWeight: '500',
+  },
+  musicControlsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '60%',
+    marginTop: 10,
+  },
+  footer: {
+    width: width,
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderTopColor: '#393E45',
+    borderWidth: 1,
+  },
+
+  iconWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  elevation: { 
+    elevation: 5,
+    shadowOffset: {
+      width: 5,
+      height: 5,
     },
-    main:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderWidth: 1,
-    },
-    footer: {
-        width: width,
-        alignItems: 'center',
-        paddingVertical: 15,
-        borderTopColor: '#393E45',
-        borderWidth: 1,
-    },
-    iconWrapper: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      width: '80%',
-    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84
+  }
 })
